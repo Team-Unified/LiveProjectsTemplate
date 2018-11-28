d5f50f2e9f587cc5eddaa90d9828b3de5d87a1e1
Kasim Hussain <kasiimh1@gmail.com>
2018-11-28 17:21:21 +0000

    Added Ionic v1 Authentication Prototype App

--- investigations/code/AuthTestApp/www/js/app.js
+++ investigations/code/AuthTestApp/www/js/app.js
@@ -1,27 +1,79 @@
-var authExample = angular.module('starter', ['ionic', 'ngCordova','ngCordovaOauth'])
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('starter', ['ionic', 'ngCordovaOauth'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-
-    console.log("platform is ready")
-
-authExample.controller("authExample", function($scope, $cordovaOauth) {   
-    $scope.login = function() {
-        $cordovaOauth.Instagram("1eee6d9118fc4221be7049d1c59c47c5",["basic"]).then(function(result) {
-          this.status = "Success, you're now connected!" + "Token:" + JSON.stringify(result);
-         // redirect_uri: 'https://tinyurl.com/krmpchb'
-        }, function(error) {
-          this.status = "ERROR " + error; 
-        });
-    }
-  })
-
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs).
+    // The reason we default this to hidden is that native apps don't usually show an accessory bar, at
+    // least on iOS. It's a dead giveaway that an app is using a Web View. However, it's sometimes
+    // useful especially with forms, though we would prefer giving the user a little more room
+    // to interact with the app.
     if (window.cordova && window.Keyboard) {
       window.Keyboard.hideKeyboardAccessoryBar(true);
     }
 
-    if (window.StatusBar) {     
+    if (window.StatusBar) {
+      // Set the statusbar to use the default style, tweak this to
+      // remove the status bar on iOS or change it to use white instead of dark colors.
       StatusBar.styleDefault();
     }
   });
 })
+.controller("MainController", function($scope, $cordovaOauth) {
+
+    status: string = "";
+    $scope.instagramLogin = function() {
+        $cordovaOauth.instagram("1eee6d9118fc4221be7049d1c59c47c5", ["basic"],["object options"]).then(function(result) {
+            console.log(JSON.stringify(result));
+            alert('Token: ' + JSON.stringify(result));
+          //  this.status = "Success, You're now logged into Instagram! " + JSON.stringify(result);
+        }, function(error) {
+            console.log(JSON.stringify(error));
+        });
+    }  
+    
+    $scope.linkedinLogin = function() {
+        $cordovaOauth.linkedin("CLIENT_ID_HERE", "CLIENT_SECRET_HERE", ["r_basicprofile", "r_emailaddress"], "my-state-data").then(function(result) {
+            console.log(JSON.stringify(result));
+        }, function(error) {
+            console.log(JSON.stringify(error));
+        });
+    }
+
+  $scope.twitterLogin = function() {
+      $cordovaOauth.twitter("CLIENT_ID_HERE", "CLIENT_SECRET_HERE").then(function(result) {
+          console.log(JSON.stringify(result));
+      }, function(error) {
+          console.log(JSON.stringify(error));
+      });
+  }
+
+  $scope.facebookLogin = function() {
+      $cordovaOauth.facebook("CLIENT_ID_HERE", ["email"], {"auth_type": "rerequest"}).then(function(result) {
+          console.log(JSON.stringify(result));
+      }, function(error) {
+          console.log(JSON.stringify(error));
+      });
+  }
+
+  $scope.redditLogin = function() {
+    $cordovaOauth.reddit("clientId", "clientSecret", ["appScope"], ["object options"]).then(function(result) {
+        console.log(JSON.stringify(result));
+    }, function(error) {
+        console.log(JSON.stringify(error));
+    });
+}
+
+$scope.googleLogin = function() {
+    $cordovaOauth.google("clientId", ["appScope"], ["object options"]).then(function(result) {
+        console.log(JSON.stringify(result));
+    }, function(error) {
+        console.log(JSON.stringify(error));
+    });
+}
+});
