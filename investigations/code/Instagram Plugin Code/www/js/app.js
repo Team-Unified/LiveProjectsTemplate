c53679807e0b3cb761a5625de9bc57d988ed7627
Kasim Hussain <kasiimh1@gmail.com>
2018-12-18 01:53:27 +0000

    New changes, still need a ngFor for the displaying on the home page

--- investigations/code/Instagram Plugin Code/www/js/app.js
+++ investigations/code/Instagram Plugin Code/www/js/app.js
@@ -41,8 +41,7 @@ angular.module('starter', ['ionic', 'ngCordovaOauth','ngStorage'])
 
     $scope.update = function() {
 
-        req = instagramRequest + $localStorage.instagram;
-        
+        req = instagramRequest + $localStorage.instagram; 
         const http = new XMLHttpRequest()
         http.open("GET", req)
         http.send()
@@ -52,21 +51,39 @@ angular.module('starter', ['ionic', 'ngCordovaOauth','ngStorage'])
         // so then it will loop as many times there's objects in the response
         // right now i save the response to local storage, when i finish this to work the way it should then we won't need local storage?
         
-        $localStorage.username = resp['data'][0]['user']['username']
-        $localStorage.fullname = resp['data'][0]['user']['full_name']
-        $localStorage.profile_picture = resp['data'][0]['user']['profile_picture']
-        $localStorage.id = resp['data'][0]['user']['profile_picture']
-        $localStorage.image_post = resp['data'][0]['images']['standard_resolution']['url']
-        $localStorage.caption_text = resp['data'][0]['caption']['text']
-        $localStorage.created_time = resp['data'][0]['caption']['created_time']
-    }
+        var length = [resp['data'].length];
+        var images = [length];
+        var caption_text = [length];
+        var created_time = [length];
 
+        for(let i = 0; i < resp['data'].length; i++)
+        { 
+            //if a video is added it doesnt currently work, need to fix this or add it as an enhancement
+            images[i] = resp['data'][i]['images']['standard_resolution']['url']
+            //if no caption it currently errors
+            caption_text[i] = resp['data'][i]['caption']['text']
+            //again only works if a caption is present
+            created_time[i] = new Date(parseInt (resp['data'][i]['caption']['created_time']) * 1000).toLocaleString()
+        }
+
+        $localStorage.image_post = images;
+        $localStorage.caption_text = caption_text;
+        $localStorage.created_time = created_time;
+
+        //hardcoded to pull this from the first entry in the array since it only needs to be fetched once
+        //only fetch if the localstorage is null, since these values can be reused
+        while ($localStorage.username == null && $localStorage.fullname == null && $localStorage.profile_picture == null && $localStorage.id == null)
+        {
+            $localStorage.username = resp['data'][0]['user']['username']
+            $localStorage.fullname = resp['data'][0]['user']['full_name']
+            $localStorage.profile_picture = resp['data'][0]['user']['profile_picture']
+            $localStorage.id = resp['data'][0]['user']['profile_picture']
+        }
+    }
 
     $scope.resetAll = function() {
     alert('Deleting saved tokens for all Accounts!');
-    $localStorage.$reset({
-        instagram: reset
-    });
+    $localStorage.$reset();
 }
 
 });
