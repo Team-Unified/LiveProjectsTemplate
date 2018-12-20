e05b9060c3ac20c65d9d3f4b1da65b3782ba418f
Kasim Hussain <kasiimh1@gmail.com>
2018-12-20 13:31:51 +0000

    Fixed Linkedin & Twitter

--- investigations/code/LocalStorageTest/www/js/app.js
+++ investigations/code/LocalStorageTest/www/js/app.js
@@ -1,25 +1,14 @@
-// Ionic Starter App
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic', 'ngCordovaOauth','ngStorage'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs).
-    // The reason we default this to hidden is that native apps don't usually show an accessory bar, at
-    // least on iOS. It's a dead giveaway that an app is using a Web View. However, it's sometimes
-    // useful especially with forms, though we would prefer giving the user a little more room
-    // to interact with the app.
     if (window.cordova && window.Keyboard) {
       window.Keyboard.hideKeyboardAccessoryBar(true);
     }
 
     if (window.StatusBar) {
-      // Set the statusbar to use the default style, tweak this to
-      // remove the status bar on iOS or change it to use white instead of dark colors.
+
       StatusBar.styleDefault();
     }
   });
@@ -30,8 +19,7 @@ angular.module('starter', ['ionic', 'ngCordovaOauth','ngStorage'])
     $localStorage
     ){
 
-        var empty = "No Token";
-        var reset = "Token Reset";
+    var empty = "No Token";
 
     $scope.$storage = $localStorage.$default({
         instagram: empty,
@@ -51,7 +39,7 @@ angular.module('starter', ['ionic', 'ngCordovaOauth','ngStorage'])
     }  
 
     $scope.linkedinLogin = function() {
-        $cordovaOauth.linkedin("CLIENT_ID_HERE", "CLIENT_SECRET_HERE", ["r_basicprofile", "r_emailaddress"], "my-state-data").then(function(result) {
+        $cordovaOauth.linkedin("CLIENT_ID", "CLIENT_SECRET", ["r_basicprofile", "r_emailaddress"], "STATE_STRING").then(function(result) {
             console.log(JSON.stringify(result));
             $localStorage.linkedin = JSON.stringify(result);
         }, function(error) {
@@ -59,15 +47,23 @@ angular.module('starter', ['ionic', 'ngCordovaOauth','ngStorage'])
         });
     }
 
-    $scope.facebookLogin = function() {
-      $cordovaOauth.facebook("CLIENT_ID_HERE", ["email"], {"auth_type": "rerequest"}).then(function(result) {
+    $scope.twitterLogin = function() {
+      $cordovaOauth.twitter("CLIENT_ID","CLIENT_SECRET").then(function(result) {
           console.log(JSON.stringify(result));
-          $localStorage.facebook = JSON.stringify(result);
+          $localStorage.twitter = JSON.stringify(result);
       }, function(error) {
           console.log(JSON.stringify(error));
       });
   }
 
+  $scope.facebookLogin = function() {
+      $cordovaOauth.facebook("CLIENT_ID_HERE", ["email"], {"auth_type": "rerequest"}).then(function(result) {
+        console.log(JSON.stringify(result));
+        $localStorage.twitter = JSON.stringify(result);
+    }, function(error) {
+        console.log(JSON.stringify(error));
+    });
+}
     $scope.redditLogin = function() {
     $cordovaOauth.reddit("clientId", "clientSecret", ["appScope"], ["object options"]).then(function(result) {
         console.log(JSON.stringify(result));
@@ -79,12 +75,7 @@ angular.module('starter', ['ionic', 'ngCordovaOauth','ngStorage'])
 
     $scope.resetAll = function() {
     alert('Deleting saved tokens for all Accounts!');
-    $localStorage.$reset({
-        instagram: reset,
-        linkedin: reset,
-        facebook: reset,
-        reddit: reset
-    });
+    $localStorage.$reset();
 }
 
 });
