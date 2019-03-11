59351bfbf33700fe919cbbce5bc2d10edfe46de3
Kasim Hussain <kasiimh1@gmail.com>
2019-03-11 16:32:53 +0000

    Push Working Feed for both instagram and reddit, you need to sign into Instagram before it shows anything currently

--- code/Collate My Social Feed/collate-my-social-feed/www/js/app.js
+++ code/Collate My Social Feed/collate-my-social-feed/www/js/app.js
@@ -22,7 +22,8 @@ angular.module('starter', ['ngStorage', 'ngCordovaOauth', 'ionic'])
         var instagramRequest = 'https://api.instagram.com/v1/users/self/media/recent/?access_token=';
         var redditRequest = "https://api.reddit.com/r/";    
         var subreddit = "";
-        var resp;
+        var instagramResp
+        var redditResp; 
 
         //instagram login
         $scope.instagramLogin = function () {
@@ -46,32 +47,32 @@ angular.module('starter', ['ngStorage', 'ngCordovaOauth', 'ionic'])
             var xhr = new XMLHttpRequest()
             xhr.open("GET", req)
             xhr.send()
-            xhr.onload = () => resp = JSON.parse(xhr.response);
+            xhr.onload = () => instagramResp = JSON.parse(xhr.response);
 
             // this needs to be done in like a ForEach or something along them lines for how it works for Instagram, 
             // so then it will loop as many times there's objects in the response
             // right now i save the response to local storage, when i finish this to work the way it should then we won't need local storage?
 
-            var length = [resp['data'].length];
+            var length = [instagramResp['data'].length];
             var images = [length];
             var caption_text = [length];
             var created_time = [length];
 
-            for (let i = 0; i < resp['data'].length; i++) {
+            for (let i = 0; i < length; i++) {
                 //if a video is added it doesnt currently work, need to fix this or add it as an enhancement
-                images[i] = resp['data'][i]['images']['standard_resolution']['url']
+                images[i] = instagramResp['data'][i]['images']['standard_resolution']['url']
 
                 //checks if the created_time exists at this path (only for no caption posts), if not it uses the path if the post has a caption
-                if (!resp['data'][i]['created_time']) {
-                    created_time[i] = new Date(parseInt(resp['data'][i]['caption']['created_time']) * 1000).toLocaleString()
+                if (!instagramResp['data'][i]['created_time']) {
+                    created_time[i] = new Date(parseInt(instagramResp['data'][i]['caption']['created_time']) * 1000).toLocaleString()
                 }
-                else { created_time[i] = new Date(parseInt(resp['data'][i]['created_time']) * 1000).toLocaleString() }
+                else { created_time[i] = new Date(parseInt(instagramResp['data'][i]['created_time']) * 1000).toLocaleString() }
 
                 //checks if the caption exists at this path (only for no caption posts), if not it uses the path if the post has a caption
-                if (!resp['data'][i]['caption']) {
+                if (!instagramResp['data'][i]['caption']) {
                     caption_text[i] = "No Caption Available"
                 }
-                else { caption_text[i] = resp['data'][i]['caption']['text'] }
+                else { caption_text[i] = instagramResp['data'][i]['caption']['text'] }
             }
 
             $localStorage.image_post = images;
@@ -81,10 +82,10 @@ angular.module('starter', ['ngStorage', 'ngCordovaOauth', 'ionic'])
             //hardcoded to pull this from the first entry in the array since it only needs to be fetched once
             //only fetch if the localstorage is null, since these values can be reused
             while ($localStorage.username == null && $localStorage.fullname == null && $localStorage.profile_picture == null && $localStorage.id == null) {
-                $localStorage.username = resp['data'][0]['user']['username']
-                $localStorage.fullname = resp['data'][0]['user']['full_name']
-                $localStorage.profile_picture = resp['data'][0]['user']['profile_picture']
-                $localStorage.id = resp['data'][0]['user']['profile_picture']
+                $localStorage.username = instagramResp['data'][0]['user']['username']
+                $localStorage.fullname = instagramResp['data'][0]['user']['full_name']
+                $localStorage.profile_picture = instagramResp['data'][0]['user']['profile_picture']
+                $localStorage.id = instagramResp['data'][0]['user']['profile_picture']
             }
         }
 
@@ -123,6 +124,12 @@ angular.module('starter', ['ngStorage', 'ngCordovaOauth', 'ionic'])
         $scope.redditUpdate = function () {
             //set the var to that of which input tag is 
             subreddit = angular.element('#query').val()
+
+            if (subreddit == "null" || subreddit.length < 1)
+            {
+                subreddit = "funny"
+            }
+
             //sitch together req url, subreddit, and top to JSON 
             req = redditRequest + subreddit + "/top.json?";
             //used for debug, making sure the URL is joined together as expected
@@ -130,7 +137,39 @@ angular.module('starter', ['ngStorage', 'ngCordovaOauth', 'ionic'])
             var xhr = new XMLHttpRequest()
             xhr.open("GET", req)
             xhr.send()
-            xhr.onload = () => resp = JSON.parse(xhr.response);  
+            xhr.onload = () => redditResp = JSON.parse(xhr.response);  
+
+            var length = [redditResp['data']['children'].length];
+
+            console.log("Resp children length is = " + length)
+            for (let i = 0; i < length; i++) //limit post to two right now for testing, set back to length
+            {
+                console.log("Post Title is = " + redditResp['data']['children'][i]['data']['title'] +
+                "\nURL is = https://reddit.com/" + redditResp['data']['children'][i]['data']['permalink'] + 
+                "\nSubreddit is = " + redditResp['data']['children'][i]['data']['thumbnail'])
+            }
+
+            /* if thumbnail url == self then there's no thumbnail for that post, maybe only text! */
+
+            var title = [length];
+            var url = [length];
+            var thumbnail = [length];
+
+            for (let i = 0; i < length; i++) {
+                //if a video is added it doesnt currently work, need to fix this or add it as an enhancement
+                    title[i] = "" + [redditResp['data']['children'][i]['data']['title']];
+                    if ((thumbnail[i] = "" + [redditResp['data']['children'][i]['data']['thumbnail']] == "self"))
+                    {
+                        thumbnail[i] = "Post has no image"; 
+
+                    }
+                    else
+                    thumbnail[i] = "" + [redditResp['data']['children'][i]['data']['thumbnail']]; 
+                    url[i] = "" + [redditResp['data']['children'][i]['data']['url']];
+            }
+            $localStorage.title = title;
+            $localStorage.thumbnail_post = thumbnail;
+            $localStorage.url = url;
         }
 
         //facebook login
